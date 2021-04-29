976534864bbc1289465d255ab1ed48d276cf7aae
rud-y <rudolf.zachar@gmail.com>
2021-04-29 17:11:43 +0100

    Add deploy job

--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -1,31 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import Chart from '../components/Chart';
-import TitleBar from '../components/TitleBar';
+import React, { useEffect, useState } from "react";
+import Chart from "../components/Chart";
+import TitleBar from "../components/TitleBar";
 
 const ChartContainer = ({ genres }) => {
   const [songs, setSongs] = useState([]);
 
   useEffect(() => {
     loadSongs(genres[0].url);
-  }, [genres])
+  }, []);
 
-  const loadSongs = url => {
+  const loadSongs = (url) => {
     fetch(url)
-      .then(res => res.json())
-      .then(songsList => setSongs(songsList.feed.entry))
-      .catch(err => console.error(err));
-  }
+      .then((res) => res.json())
+      .then((songsList) => setSongs(songsList.feed.entry))
+      .catch((err) => console.error(err));
+  };
 
-  const handleSelectChange = event => {
+  const handleSelectChange = (event) => {
     loadSongs(event.target.value);
-  }
+  };
 
   return (
     <>
-      <TitleBar
-        handleSelectChange={handleSelectChange}
-        genres={genres}
-      />
+      <TitleBar handleSelectChange={handleSelectChange} genres={genres} />
       <Chart
         songs={songs}
         url={genres[0].url}
@@ -33,6 +30,6 @@ const ChartContainer = ({ genres }) => {
       />
     </>
   );
-}
+};
 
 export default ChartContainer;
